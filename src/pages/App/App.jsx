069811f713afd9f00c0441151e256b6ba934404c
patwyzlic <patrickwyzlic@gmail.com>
2022-07-05 18:34:18 -0400

    Movies page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             <Route path="/" element={<ProfilePage/>} />
-            <Route path="/home" element={<IndexPage/>} />
+            <Route path="/movies" element={<IndexPage/>} />
             <Route path="/tvshows" element={<TVShowPage/>} />
             {/* <Route path="/orders/new" element={<NewOrderPage />} />
             <Route path="/orders" element={<OrderHistoryPage />} /> */}
