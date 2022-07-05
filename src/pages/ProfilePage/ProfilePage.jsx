950aea734c38bd949abf22cb5b016699613b04ec
poojad2101 <davepooja2101@gmail.com>
2022-07-05 20:03:06 +0530

    manage profile button  and created profile form

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,28 @@
-
+import React, { useState } from "react"
 
 export default function ProfilePage() {
 
-   
+    const [visible, setVisible] = useState(false)
     return(
 
     
-        <>
-            <h1>OrderHistoryPage</h1>
+        <div>
+            <h1>Who's Watching?</h1>
+
+            <a className="text-standards hover" onClick={() => setVisible(!visible)}>{visible ? '-' : '+'}</a>
+                {visible && 
+                    <div>
+                        <form className="form">
+                            <input type="text"
+                            placeholder="Create Profile"></input>
+                            <input type="submit" className="hover"></input>
+                        </form>
+                    </div> }
+
+
+            <button>Manage Profiles</button>
             
-        </>
+        </div>
         
     );
   }
