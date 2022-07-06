86f6484fc10631b60edb275d17126b97c2fb08e0
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-07-06 16:24:46 -0400

    um

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import "./Modal.css"
+import genres from "../genres"
 
 export default function MyVerticallyCenteredModal(props) {
     return (
