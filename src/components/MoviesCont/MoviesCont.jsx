56a4f2aff505daf1b04398e350fe1fe04e2faf62
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-07-06 14:35:37 -0400

    sign up down, but bootsrap modal on click movieCont working and passing in movie details

--- src/components/MoviesCont/MoviesCont.jsx
+++ src/components/MoviesCont/MoviesCont.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import MyVerticallyCenteredModal from "../Modal/Modal"
 import "./MoviesCont.css"
 
 
@@ -6,6 +7,9 @@ import "./MoviesCont.css"
 export default function MoviesCont({API_KEY, SetMovies, movies}){
 
   const mainUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=Action&with_watch_monetization_types=flatrate`;
+
+  const [modalShow, setModalShow] = useState(false);
+  const [clickedMovie, setClickedMovie] = useState("")
 
   function getMovies(url) {
     fetch(url)
@@ -20,6 +24,12 @@ export default function MoviesCont({API_KEY, SetMovies, movies}){
     getMovies(mainUrl)
   }, []);
 
+  function clickedMovies(evt){
+    setModalShow(true)
+    setClickedMovie(evt)
+    console.log(clickedMovie)
+  }
+
     return(
         <>
         <h4 className="row-Title">Featured Films</h4>
@@ -30,7 +40,13 @@ export default function MoviesCont({API_KEY, SetMovies, movies}){
               className="movie"
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt=""
-            />
+              onClick={() => clickedMovies(movie)}
+            /> 
+            <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            clickedmovie={clickedMovie}
+          />
           </div>
           })}
         </div>
