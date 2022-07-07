859162f6e9323b75a18721d3487456d854ee891d
patwyzlic <patrickwyzlic@gmail.com>
2022-07-06 23:28:16 -0400

    Input Handler and Newest Input Added

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -10,24 +10,41 @@ import ProfilePage from '../ProfilePage/ProfilePage';
 import TVShowPage from '../TVShowPage/TVShowPage';
 import HomePage from "../HomePage/HomePage"
 
+let newestInput = "";
+
+export function newestInputFunction(){
+  let theNewInput = newestInput
+  return theNewInput
+}
+
 export default function App() {
   const [user, setUser] = useState(getUser())
   console.log("app.js User:", user)
   const [clickedProfile, setClickedProfile] = useState("")
   const [listName, setListName] = useState("")
 
+  const [inputText, setInputText] = useState("");
+
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        let lowerCase = e.target.value.toLowerCase().split(' ').join('');
+        console.log(lowerCase)
+        newestInput = lowerCase
+        setInputText(lowerCase);
+    };
+
   return (
     <main className="App">
       {user ?
       <>
-          <NavBar user={user} setUser={setUser} clickedProfile={clickedProfile} />
+          <NavBar user={user} setUser={setUser} clickedProfile={clickedProfile} inputText={inputText} setInputText={setInputText} inputHandler={inputHandler} />
           <Routes>
             <Route path="/profiles" element={<ProfilePage profiles={user.Profiles} clickedProfile={clickedProfile} setClickedProfile={setClickedProfile}/>}/>
             {/* <Route path="/" element={<ProfilePage/>} /> */}
       
             <Route path="/home" element={<HomePage/>} />
             <Route path="/movies" element={<MoviePage/>} />
-            <Route path="/tvshows" element={<TVShowPage/>} />
+            <Route path="/tvshows" element={<TVShowPage currentText={inputHandler}/>} />
             {/* <Route path="/orders/new" element={<NewOrderPage />} />
             <Route path="/orders" element={<OrderHistoryPage />} /> */}
             <Route path="/watchlistpage" element={<WatchListPage listName={setListName}/>} />
