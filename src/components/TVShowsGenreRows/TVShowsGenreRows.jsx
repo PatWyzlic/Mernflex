8ed8110933734e75808cca855e57ba17045db556
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-07-07 08:47:50 -0400

    a

--- src/components/TVShowsGenreRows/TVShowsGenreRows.jsx
+++ src/components/TVShowsGenreRows/TVShowsGenreRows.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import * as App from "../../pages/App/App"
 import MyVerticallyCenteredModal from "../Modal/Modaltv";
-import "../TVShowsCont/TVShowsCont.css"
 
 
 export default function TVShowsGenreRows({ API_KEY, genreId, genreName}){
