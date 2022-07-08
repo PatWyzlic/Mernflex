d3aa25875b73ef5f14f6097e9ea2cc201d136025
patwyzlic <patrickwyzlic@gmail.com>
2022-07-08 10:17:25 -0400

    testing

--- setupProxy.js
+++ setupProxy.js
@@ -1,4 +1,4 @@
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {  app.use(    createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:3001" })  );};
+module.exports = function(app) {  app.use( createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:3000" })  );};
