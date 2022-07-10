3c2a77ac4ad6d74d1a57a6be2f9f94179e9f5a05
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-07-10 13:32:03 -0400

    Get rout working, at mvp

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,14 +3,14 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 import "./AuthPage.css"
 import { useState } from "react";
 
-export default function AuthPage({ setUser }) {
+export default function AuthPage({ setUser, user }) {
   const [showLogin, setShowLogin] = useState(true);
 
   return (
     <main className="AuthPage">
       {/* {showLogin ? <h1>LogIn</h1> : <h1>SignUp</h1>} */}
       <div className="flex">
-      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      {showLogin ? <LoginForm setUser={setUser} user={user} /> : <SignUpForm setUser={setUser} user={user} />}
       </div>
       <button className="authButton" onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</button>
     </main> 
