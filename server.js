e36fa359766c244f8e74f0883f3c65852885b06e
patwyzlic <patrickwyzlic@gmail.com>
2022-07-10 11:08:02 -0400

    Update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(logger('dev'))
 app.use(express.json())
 
-
+//
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
