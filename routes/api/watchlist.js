28930d327d16ab7015ec5580f2ab36282f86fd29
patwyzlic <patrickwyzlic@gmail.com>
2022-07-10 19:27:45 -0400

    Using id for watchlistitem

--- routes/api/watchlist.js
+++ routes/api/watchlist.js
@@ -4,4 +4,6 @@ const watchlistCtrl = require("../../controllers/api/watchlistController")
 
 
 // POST /API/watchlist CONTROLLER
-router.post("/add",watchlistCtrl.createwatchlistitem)
+router.post("/add/:id", watchlistCtrl.createWatchListItem)
+
+module.exports = router
