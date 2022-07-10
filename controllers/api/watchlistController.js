6c0068df1bdc1bd5e802e193fac2905215fc60b0
patwyzlic <patrickwyzlic@gmail.com>
2022-07-10 19:27:21 -0400

    Setup for creating watchlist item

--- controllers/api/watchlistController.js
+++ controllers/api/watchlistController.js
@@ -2,8 +2,24 @@ const Profile = require("../../models/profileModel")
 const CheckToken = require("../../controllers/api/users")
 const User = require("../../models/user");
 
-
 // Add an item to watchList
-function createwatchlistitem(req, res){
-    console.log(req)
+function createWatchListItem(req, res){
+        console.log("Create Watchlist Item hit")
+        console.log(req.body)
+        try{
+            // const updatedProfile = await  Profile.findByIdAndUpdate(req.params.id, req.body, {new: true})
+            // res.json(updatedProfile)
+            //const profile = await Profile.findById(req.params.id)
+            //profile.ProfileName = req.body.name
+            //profile.save()
+            //res.json(profile)
+            console.log("Try")
+        }
+    catch{
+        console.log(error)
+    }
 }
+
+module.exports = {
+    createWatchListItem
+} 
