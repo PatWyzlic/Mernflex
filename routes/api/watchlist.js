0367199ebd0ed04318ccaab1ecfb008451962167
patwyzlic <patrickwyzlic@gmail.com>
2022-07-10 23:23:11 -0400

    Update to id route

--- routes/api/watchlist.js
+++ routes/api/watchlist.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const watchlistCtrl = require("../../controllers/api/watchlistController")
+const profileCtrl = require("../../controllers/api/profileController")
 
+router.get("/watchlist/:id", profileCtrl.getUser)
 
 // POST /API/watchlist CONTROLLER
 router.post("/add/:id", watchlistCtrl.createWatchListItem)
